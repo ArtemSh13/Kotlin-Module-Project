76541e20c3f0985bca613b2d2850a0edc2d02942
ArtemSh13 <mozg1303@ya.ru>
2024-11-03 02:27:31 +0300

    Add methods to create and add archive to storage

--- src/main/kotlin/Storage.kt
+++ src/main/kotlin/Storage.kt
@@ -4,13 +4,35 @@ class Storage(
 ) {
     private val firstMenuCommand: String = "Создать архив"
 
+    private fun createArchive() : Archive {
+        val nameInputError: String = "Ошибка. Название архива не может быть пустой строкой"
+        var name: String
+
+        while (true) {
+            print("Введите название архива: > ")
+            name = readln()
+            if (name.isEmpty()) {
+                println(nameInputError)
+            } else {
+                break
+            }
+        }
+
+        return Archive(name, ArrayList())
+    }
+
+    private fun addArchive(archive: Archive) {
+        this.archives.add(archive)
+        println("Архив \"${archive.name}\" успешно создан")
+    }
+
     fun getStorageMenu(): MutableList<Pair<String, () -> Unit>> {
         val result: MutableList<Pair<String, () -> Unit>> = ArrayList()
 
         result.add(
             Pair(
                 this.firstMenuCommand,
-                { println("Вызвана команда ${this.firstMenuCommand}") })
+                { this.addArchive(this.createArchive()) })
         )
 
         for (archive in this.archives) {
