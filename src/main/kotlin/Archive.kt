6da8bfed357676f03bcc3024857d96030bb98abe
ArtemSh13 <mozg1303@ya.ru>
2024-11-03 00:45:46 +0300

    Add method to create and add note to archive

--- src/main/kotlin/Archive.kt
+++ src/main/kotlin/Archive.kt
@@ -4,17 +4,50 @@ data class Archive(
 ) {
     private val firstMenuCommand: String = "Создать заметку"
 
+    private fun createNote() : Note {
+        val nameInputError: String = "Ошибка. Название заметки не может быть пустой строкой"
+        val contentInputError: String = "Ошибка. Заметка не может быть пустой строкой"
+        var name: String = ""
+        var content: String = ""
+
+        while (true) {
+            print("Введите название заметки: > ")
+            name = readln()
+            if (name.isEmpty()) {
+                println(nameInputError)
+            } else {
+                break
+            }
+        }
+        while (true) {
+            println("Введите текст заметки:")
+            content = readln()
+            if (name.isEmpty()) {
+                println(contentInputError)
+            } else {
+                break
+            }
+        }
+
+        return Note(name, content)
+    }
+
+    private fun addNote(note: Note) {
+        this.notes.add(note)
+        println("Заметка \"${note.name}\" добавлена в архив \"${this.name}\"")
+    }
+
     fun getArchiveMenu(): MutableList<Pair<String, () -> Unit>> {
         val result: MutableList<Pair<String, () -> Unit>> = ArrayList()
 
         result.add(
             Pair(
                 this.firstMenuCommand,
-                { println("Вызвана команда ${this.firstMenuCommand}") })
+                { this.addNote(this.createNote()) })
         )
 
         for (note in this.notes) {
-            result.add(Pair(note.name, { println("Открыта заметка ${note.name}") }))
+            result.add(Pair("Открыть заметку \"${note.name}\"", { note.printNote() } ))
         }
         return result
     }
