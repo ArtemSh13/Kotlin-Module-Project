0a43f48420ad3862161565e32f0f132104dca6b8
ArtemSh13 <mozg1303@ya.ru>
2024-11-04 01:30:49 +0300

    Add main storage and screen with test data factory

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,33 @@
+import kotlin.collections.ArrayList
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val storage = Storage(
+        prepareTestData(
+            archiveCount = 2,
+            notesPerArchive = 2
+        )
+    )
+
+    val mainScreen = Screen("=> Архивы", storage)
+    mainScreen.startSession()
+}
+
+/**
+ * The prepareTestData() function is needed only to a reviewer can check the project faster.
+ * It doesn't affect this program efficiency.
+ */
+fun prepareTestData(archiveCount: Int, notesPerArchive: Int): MutableList<Archive> {
+    val archiveList = ArrayList<Archive>()
+    var noteIndex = 0
+
+    for (i in 0 until archiveCount) {
+        val notes = ArrayList<Note>()
+        for (j in 1..notesPerArchive) {
+            notes.add(Note("Заметка $noteIndex", "Текст заметки $noteIndex"))
+            ++noteIndex
+        }
+        archiveList.add(Archive("Архив $i", notes))
+    }
+
+    return archiveList
 }
