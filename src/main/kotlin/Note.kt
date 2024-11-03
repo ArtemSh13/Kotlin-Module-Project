1a150cec838cf9f7843786be0914bfcce4cd1f31
ArtemSh13 <mozg1303@ya.ru>
2024-11-03 20:59:42 +0300

    Add line break to Note divider

--- src/main/kotlin/Note.kt
+++ src/main/kotlin/Note.kt
@@ -2,7 +2,7 @@ data class Note(
     val name: String,
     val content: String
 ) {
-    private val divider: String = "==========Конец заметки=========="
+    private val divider: String = "==========Конец заметки==========\n"
 
     fun printNote() {
         println("Заметка \"${this.name}\" : ")
